735715cba1f80fd2b887476a808ba3335fc43992
t7m3 <toda7.mk3@gmail.com>
2020-11-15 12:12:18 +0900

    2020-11-15(日)　 1回目のコミット 爆発をGifアニメーション画像にした。（連射可能版）

--- app/src/main/java/com/example/simpleshootinggame_v401/MainActivity.kt
+++ app/src/main/java/com/example/simpleshootinggame_v401/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 爆発gifアニメーションの imageView を用意する
+        Glide.with(this).load(R.drawable.daibakuhatsu).into(imageViewGif)
+        imageViewGif.visibility = View.INVISIBLE
 
         // スクリーンの幅と高さを取得する
         val dMetrics = DisplayMetrics()  //DisplayMetrics のインスタンスを生成する
@@ -85,13 +90,13 @@ class MainActivity : AppCompatActivity() {
 
                         enemy.state = "stop"  // 敵の移動を止める
 
-                        enemy.imageView.setImageResource(R.drawable.s5z8k0g6)  //画像を爆発の画像に変える
+                        //enemy.imageView.setImageResource(R.drawable.s5z8k0g6)  //画像を爆発の画像に変える
 
                         // 敵を不可視にし、爆発gifアニメーション画像をその場所に表示する
-                        //enemy.imageView.visibility = View.INVISIBLE
-                        //imageViewGif.x = enemy.imageView.x
-                        //imageViewGif.y = enemy.imageView.y
-                        //imageViewGif.visibility = View.VISIBLE
+                        enemy.imageView.visibility = View.INVISIBLE
+                        imageViewGif.x = enemy.imageView.x
+                        imageViewGif.y = enemy.imageView.y
+                        imageViewGif.visibility = View.VISIBLE
 
                         explosion_millisUntilFinished =
                             millisUntilFinished// 爆発したときの時刻（のようなもの）を保存しておく
@@ -107,11 +112,11 @@ class MainActivity : AppCompatActivity() {
                 if (explosion_millisUntilFinished - millisUntilFinished >= 4000) {  // 爆発の時間が経過したら
 
                     // imageViewEnemyの画像をロケットの画像に変える
-                    enemy.imageView.setImageResource(R.drawable.rocket)
+                    //enemy.imageView.setImageResource(R.drawable.rocket)
 
                     // 爆発gifアニメーション画像を不可視にし、敵の画像を可視にする
-                    //imageViewGif.visibility = View.INVISIBLE
-                    //enemy.imageView.visibility = View.VISIBLE
+                    imageViewGif.visibility = View.INVISIBLE
+                    enemy.imageView.visibility = View.VISIBLE
 
                     enemy.state = "move"  // imageViewEnemy が移動する
                 }
